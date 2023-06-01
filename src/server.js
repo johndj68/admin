551925e5a002189d27d340dae81728d578d7bad0
john <klbj368@gmail.com>
2023-06-01 15:52:07 -0300

    create server

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import AdminJS from 'adminjs';
+import AdminJSExpress from '@adminjs/express';
+import express from 'express';
+
+const app = express();
+const adminJS = new AdminJS({
+    databases: [],
+    rootPath: '/admin',
+    resources:[]
+});
+
+const router = AdminJSExpress.buildRouter(adminJS);
+
+app.use(adminJS.options.rootPath, router);
+app.listen(5000, () => {
+    console.log('AdminJS is under http://localhost:5000/admin')
+});
